d26026d0317cd955469535b991dd8469835af820
Melina Corbalan <melina.corbalan@gmail.com>
2022-01-11 19:03:08 -0300

    update home icon & media queries

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,12 +1,19 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import homeIcon from '../assets/home-icon-silhouette.png'
 
 const Nav = () => {
     return (
-        <nav className="nav" id="home">
-            <Link id="about" to="/melinacorbalan/aboutme">About me</Link>
-            <Link id="projects" to="/melinacorbalan/projects">Projects</Link>
-            <Link id="contact" to="/melinacorbalan/contact">Contact</Link>
+        <nav className="head">
+            <Link to="/melinacorbalan" className="iconHome">
+                <img src={homeIcon} alt="home icon"/>
+            </Link>
+            <div className="nav">
+                <Link id="about" to="/melinacorbalan/aboutme">About me</Link>
+                <Link id="projects" to="/melinacorbalan/projects">Projects</Link>
+                <Link id="contact" to="/melinacorbalan/contact">Contact</Link>
+            </div>
+
         </nav>
     )
 }
